cc6f247939d26c94654e72f8c36c6ae3de28496c
DaYuan <sduum82@gmail.com>
2020-07-07 03:24:26 +0800

    add Layout

---
+++ src/Layout/T1B2.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="DashBoard">
+    <div id="Top10">sasasasasasasasasasa</div>
+    <div id="Buttom10">
+      <!-- sasasasasasasasasasa -->
+      <div id="Buttom5R">
+        asd
+        <h4>DASHBOARD</h4>
+      </div>
+      <div id="Buttom5L">
+        asd
+        <h4>DASHBOARD</h4>
+      </div>
+      <!-- <div style="flex-grow:2">item 3</div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashBoard",
+  components: {}
+};
+</script>
+
+<style>
+#DashBoard {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex: 1;
+
+  flex-direction: column;
+  overflow: scroll;
+  /* background: #ff2345; */
+}
+#Buttom5L {
+  height: 350px;
+  background: #fff;
+  margin-left: 10px;
+  display: flex;
+  flex: 1;
+  -webkit-align-items: center;
+  -webkit-justify-content: center;
+  align-items: center;
+  justify-content: center;
+}
+#Buttom5R {
+  height: 350px;
+  background: #fff;
+  margin-right: 10px;
+
+  display: flex;
+  flex: 1;
+  -webkit-align-items: center;
+  -webkit-justify-content: center;
+  align-items: center;
+  justify-content: center;
+}
+#Top10 {
+  background: #fff;
+  margin: 0px 20px 10px 20px;
+  display: flex;
+  flex: 1;
+  /* flex-direction: row;
+  -webkit-align-items: center;
+  -webkit-justify-content: center;
+  align-items: center;
+  justify-content: center; */
+}
+#Buttom10 {
+  /* background: #fff; */
+  margin: 10px 20px 50px 20px;
+  display: flex;
+  flex: 1;
+  /* flex-direction: row;
+  -webkit-align-items: center;
+  -webkit-justify-content: center;
+  align-items: center;
+  justify-content: center; */
+  flex-wrap: wrap;
+}
+</style>
