2c03761f8c6d586a77ff314f254ed166cfd29250
DaYuan <sduum82@gmail.com>
2020-07-07 03:24:50 +0800

    add NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="App">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <router-view></router-view>
-
+    <div id="NavBar">
+      <div id="Logo">
+        <img alt="LogBoard logo" src="./assets/LogBoardLOGO.svg" width="130" />
+      </div>
+      <div id="NavItems">
+        <!-- <div id="Item">
+          <img id="Icon" alt="LogBoard Icon" src="./assets/icon/DASHBOARD.svg" width="32" />
+          <h4>DASHBOARD</h4>
+        </div>
+        <div id="Item">
+          <img id="Icon" alt="LogBoard Icon" src="./assets/icon/PHOTODIARY.svg" width="32" />
+          <h4>PHOTODIARY</h4>
+        </div>-->
+        <router-link :to="{ name: 'dashboard' }" class="Nav">
+          <img id="Icon" alt="LogBoard Icon" src="./assets/icon/DASHBOARD.svg" width="32" />
+          <h4>DASHBOARD</h4>
+        </router-link>
+        <router-link :to="{ name: 'photodiary' }" class="Nav">
+          <img id="Icon" alt="LogBoard Icon" src="./assets/icon/PHOTODIARY.svg" width="32" />
+          <h4>PHOTODIARY</h4>
+        </router-link>
+      </div>
+      <!-- <img alt="Vue logo" src="./assets/LogBoardLOGO.svg" /> -->
+      <!-- <img :src="image" /> -->
+    </div>
+    <div id="Content">
+      <router-view></router-view>
+    </div>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import 'reset-css';
 
 export default {
   name: "App",
@@ -20,20 +46,98 @@ export default {
   },
   data() {
     return {
-      drawer: true
+      drawer: true,
+      image: require("./assets/LogBoardLOGO.svg")
     };
   }
 };
 </script>
 
 <style>
+.Nav {
+  height: 50px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  -webkit-align-items: center;
+  -webkit-justify-content: center;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 20px;
+  text-align: center;
+  color: #fff;
+}
 #App {
+  overflow: hidden;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  background: #5c6f75;
+  display: flex;
+  flex: 1;
+  width: 100vw;
+  height: 100vh;
+}
+#NavBar {
+  /* background: #f0afff; */
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  -webkit-justify-content: center;
+  justify-content: center;
+  /* height: 100vh; */
+}
+#Logo {
+  background: #738489;
+  display: flex;
+  flex: 1;
+  -webkit-align-items: center;
+  -webkit-justify-content: center;
+  align-items: center;
+  justify-content: center;
+  /* height: 100vh; */
+}
+#Icon {
+  padding: 5px;
+}
+#NavItems {
+  background: #697b80;
+
+  display: flex;
+  flex: 8;
+  flex-direction: column;
+}
+#Item {
+  /* background: #30a0fa; */
+  /* border-block-end-width: 10px;
+  border-width: 10px; */
+  height: 50px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  -webkit-align-items: center;
+  -webkit-justify-content: center;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 20px;
+  text-align: center;
+  color: #fff;
+}
+#Content {
+  flex: 7;
+  display: flex;
+
+  /* height: 100vh; */
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
 
