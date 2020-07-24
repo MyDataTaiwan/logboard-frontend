9141104c158bade964f9b224cd8a023b2a835082
James Chien <shc261392@gmail.com>
2020-07-24 13:21:31 +0800

    Add s3-deploy plugin and generated config
    Signed-off-by: James Chien <shc261392@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,30 @@
 // };
 module.exports = {
         outputDir: 'dist',
+
         publicPath: process.env.NODE_ENV === 'production'
                 ? '/logboard-frontend/'
-                : '/'
+                : '/',
+
+        pluginOptions: {
+          s3Deploy: {
+            registry: undefined,
+            awsProfile: 's3',
+            overrideEndpoint: false,
+            region: 'us-east-1',
+            bucket: 'logboard-frontend',
+            createBucket: false,
+            staticHosting: true,
+            staticIndexPage: 'index.html',
+            staticErrorPage: 'index.html',
+            assetPath: 'dist',
+            assetMatch: '**',
+            deployPath: '/',
+            acl: 'public-read',
+            pwa: false,
+            enableCloudfront: false,
+            pluginVersion: '4.0.0-rc3',
+            uploadConcurrency: 5
+          }
+        }
 }
