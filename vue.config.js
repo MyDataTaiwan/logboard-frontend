4e539193402f78b283af28cd73f65996c3522467
James Chien <shc261392@gmail.com>
2020-08-11 05:38:34 +0800

    Add s3 deploy config, changing deploy root to /
    Signed-off-by: James Chien <shc261392@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -7,31 +7,28 @@
 //         },
 // };
 module.exports = {
-        outputDir: 'dist',
-
-        publicPath: process.env.NODE_ENV === 'production'
-                ? '/logboard-frontend/'
-                : '/',
-
-        pluginOptions: {
-          s3Deploy: {
-            registry: undefined,
-            awsProfile: 's3',
-            overrideEndpoint: false,
-            region: 'us-east-1',
-            bucket: 'logboard-frontend',
-            createBucket: false,
-            staticHosting: true,
-            staticIndexPage: 'index.html',
-            staticErrorPage: 'index.html',
-            assetPath: 'dist',
-            assetMatch: '**',
-            deployPath: '/',
-            acl: 'public-read',
-            pwa: false,
-            enableCloudfront: false,
-            pluginVersion: '4.0.0-rc3',
-            uploadConcurrency: 5
-          }
-        }
+  outputDir: 'dist',
+  publicPath: '/',
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 's3',
+      overrideEndpoint: false,
+      region: 'us-east-1',
+      bucket: 'logboard-frontend',
+      createBucket: false,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: true,
+      cloudfrontId: 'E6ZOEJYF4MFVN',
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
+  }
 }
